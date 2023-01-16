6c3f2ede11f42406398c66183899fa34ff7c21f5
yajatvishwak <yajat472001@gmail.com>
2023-01-16 23:11:45 +0530

    asd

--- src/api/blog/controllers/blog.js
+++ src/api/blog/controllers/blog.js
@@ -16,13 +16,47 @@ module.exports = createCoreController("api::blog.blog", ({ strapi }) => ({
   },
   async blogbyslug(ctx) {
     const { slug } = ctx.request.params;
+    let data = {
+      blogtitle: "",
+      blogcontent: ``,
+      authorname: "",
+      authorabout: "",
+      authordp: "",
+      publishdate: "",
+      categories: [],
+      coverpic: "",
+      authordesignation: "",
+    };
 
     // Calling the default core action
     const entries = await strapi.entityService.findMany("api::blog.blog", {
-      fields: ["title"],
-      filters: { slug: "yajat-blog" },
-      populate: { categories: true },
+      fields: ["title", "blogcontent", "createdAt"],
+      filters: { slug },
+      populate: {
+        categories: true,
+        coverpic: true,
+        author: {
+          populate: {
+            authordp: true,
+          },
+        },
+      },
     });
-    return { entries };
+    if (entries && entries.length > 0) {
+      data.categories = entries[0].categories.map((item) => item.category);
+      data.authorabout = entries[0].author.authorabout;
+      data.authorname = entries[0].author.authorname;
+      data.blogtitle = entries[0].title;
+      data.blogcontent = entries[0].blogcontent;
+      data.authordesignation = entries[0].author.authordesignation;
+      data.authordp = entries[0].author.authordp.url;
+      data.publishdate = entries[0].createdAt;
+      if (entries[0].coverpic) {
+        data.coverpic = entries[0].coverpic;
+      }
+      return { ...data };
+    } else {
+      return null;
+    }
   },
 }));
